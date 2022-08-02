c5c2ebaca8e6fcdfb650df3846a5b483ece7bac0
pael1 <rafaelfernandez677@gmail.com>
2022-08-02 21:38:46 +0800

    =front end

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Rafael Fernandez</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-md">
+      <a class="navbar-brand" href="index.php">Tasks</a>
+    </div>
+  </nav>
+  <div class="container">
+    <h5 class="text-center mb-4">Table Contents</h5>
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Part 1</h5>
+        <div class="row">
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <p class="card-text">1. Create the given pattern (double-diamond).</p>
+                <a href="./tasks/part1/task1.php" class="btn btn-primary">View</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <p class="card-text">2. Create the given pattern (x).</p>
+                <a href="./tasks/part1/task2.php" class="btn btn-primary">View</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <p class="card-text">3. Create the given pattern.</p>
+                <a href="./tasks/part1/task3.php" class="btn btn-primary">View</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <p class="card-text">4.Create the given pattern.</p>
+                <a href="/tasks/part1/task4.php" class="btn btn-primary">View</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row mt-4">
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <p class="card-text">5.Create a page that shows “refresh count: 0”.</p>
+                <a href="./tasks/part1/task5.php" class="btn btn-primary">View</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="card mt-1">
+      <div class="card-body">
+        <h5 class="card-title">Part 2</h5>
+        <div class="row">
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <p class="card-text">1. Generate a random character from a -k.</p>
+                <a href="./tasks/part2/task1.php" class="btn btn-primary">View</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <p class="card-text">2. Manipulate multidimensional array.</p>
+                <a href="./tasks/part2/task2.php" class="btn btn-primary">View</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <p class="card-text">3. Create a stack of integers using arrays( First in last out ).</p>
+                <a href="./tasks/part2/task3.php" class="btn btn-primary">View</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <p class="card-text">4. Create a queue of integers using arrays (first in first out ).</p>
+                <a href="./tasks/part2/task4.php" class="btn btn-primary">View</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row mt-4">
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <p class="card-text">5. Fix the code function to output the correct result. Explain your answer.</p>
+                <a href="./tasks/part2/task5.php" class="btn btn-primary">View</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+
+</html>
