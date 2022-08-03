8eaf519b9161e856817ef0746f09a997376e8047
pael1 <rafaelfernandez677@gmail.com>
2022-08-03 23:28:57 +0800

    Generate a random character from a -k

--- tasks/part2/task1.php
+++ tasks/part2/task1.php
@@ -1,74 +1,21 @@
 <?php
-// class CreateStack
-// {
-//     public $top;
-//     public $stack = array();
 
-//     function __construct()
-//     {
-//         $this->top = -1;
-//     }
-
-//     // create a function to check whether 
-//     // the stack is empty or not  
-//     public function isEmpty()
-//     {
-//         if ($this->top == -1) {
-//             echo "Stack is empty. <br>";
-//         } else {
-//             echo "Stack is not empty. <br>";
-//         }
-//     }
-
-//     //create a function to return size of the stack 
-//     public function size()
-//     {
-//         echo "The size of stak is " .  $this->top + 1 . "<br>";
-//     }
-
-//     //create a function to add new element 
-//     public function push($x)
-//     {
-//         $this->stack[++$this->top] = $x;
-//         echo $x . " is added into the stack. <br>";
-//     }
-
-//     //create a function to delete top element   
-//     public function pop()
-//     {
-//         if ($this->top < 0) {
-//             echo "Stack is empty. <br>";
-//         } else {
-//             $x = $this->stack[$this->top--];
-//             echo $x . " is deleted from the stack. <br>";
-//         }
-//     }
-
-//     public function topElement()
-//     {
-//         if ($this->top < 0) {
-//             echo "Stack is empty. <br>";
-//         } else {
-//             echo "The top element of stack is " . $this->stack[$this->top];
-//         }
-//     }
-// }
-
-// $MyStacks = new CreateStack();
-if(!isset($_SESSION['cart'])){
-    $_SESSION['cart'] = [];
+function generateNumber()
+{
+    $random_number_array = range('A', 'K');
+    shuffle($random_number_array);
+    //array, index, lenght
+    return array_splice($random_number_array, 0, 5, true);
 }
-session_start();
-if (isset($_POST['submit'])) { 
-    $_SESSION['cart'][] = $_POST['stack_name'];
-}
-if (isset($_POST['pop'])) { 
-   removeLast();
-}
-function removeLast(){
-    $data = count($_SESSION['cart']);
-    unset($_SESSION['cart'][$data - 1]);
-}
+
+$marksarray = array(
+    generateNumber(),
+    generateNumber(),
+    generateNumber(),
+    generateNumber()
+);
+
+print_r($marksarray);
 
 ?>
 
@@ -79,37 +26,38 @@ function removeLast(){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Part 2; Number 3</title>
+    <title>Part 2; Number 2</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../css/style.css">
 </head>
 
 <body>
+    <div class="container">
+        <pre>
+        1. Generate a random character from a -k
+            a. Create a 4 x 5 table
+            b. Display all the random character inside the table
+            c. Highlight all the character that belongs to the even column
+        </pre>
+        <table class="table table-bordered">
+            <thead>
+            </thead>
+            <tbody>
+                <?php
+                for ($i = 0; $i < count($marksarray); $i++) {
+                    echo "<tr>";
+                    echo "<td>" . $marksarray[$i][0] . "</td>";
+                    echo "<td>" . $marksarray[$i][1] . "</td>";
+                    echo "<td>" . $marksarray[$i][2] . "</td>";
+                    echo "<td>" . $marksarray[$i][3] . "</td>";
+                    echo "<td>" . $marksarray[$i][4] . "</td>";
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
 
-    <form action="task1.php" method="post">
-        Name: <input type="text" name="stack_name"><br>
-        <input type="submit" name="submit">
-        <input type="submit" name="pop" value="Pop"/>
-    </form>
-
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">Name</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-
-            foreach ($_SESSION['cart'] as $key => $value) {
-                echo "<tr>";
-                echo "<th scope='row'>" . $value . "</th>";
-                echo "</tr>";
-            }
-            ?>
-
-
-        </tbody>
-    </table>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
 
