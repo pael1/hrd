7dfbeb3557cca434f2239103f8100bf66ef62f87
pael1 <rafaelfernandez677@gmail.com>
2022-08-04 16:01:07 +0800

    added html codes

--- tasks/part2/task3.php
+++ tasks/part2/task3.php
@@ -26,22 +26,33 @@ function removeLast()
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Part 2; Number 3</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../css/style.css">
 </head>
 
 <body>
     <div class="container">
-        <pre>
+    <div class="return mb-3">
+            <a href="../../index.php" class="btn btn-secondary">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-left" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z" />
+                </svg>
+            </a>
+        </div>
+        <pre class="question">
         3.Create a stack of integers using arrays( First in last out )
 
-a. create input fields and push button to insert new value
-b. Create pop button to remove top value
-c. Always display the existing stack content
-d. Do not use pre defined php array function like array_pop
+            a. create input fields and push button to insert new value
+            b. Create pop button to remove top value
+            c. Always display the existing stack content
+            d. Do not use pre defined php array function like array_pop
         </pre>
         <form action="task3.php" method="post">
-            Name: <input type="text" name="stack_name"><br>
-            <button type="submit" name="push">Push</button>
-            <button type="submit" name="pop">Pop</button>
+            <div class="mb-3">
+                <label for="exampleInputName" class="form-label">Name</label>
+                <input type="text" class="form-control" id="exampleInputName" name="stack_name">
+            </div>
+            <button type="submit" class="btn btn-success" name="push">Push</button>
+            <button type="submit" class="btn btn-danger" name="pop">Pop</button>
         </form>
 
         <table class="table">
